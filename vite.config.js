365f388607595ea6cea44440ebb428c804e665f4
Tegar127 <mtegarfitrianoj123@gmail.com>
2026-01-13 14:40:56 +0700

    convert vue to vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    react(),
     tailwindcss(),
   ],
 })
